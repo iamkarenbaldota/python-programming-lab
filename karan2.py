924c00d4d9161495227941e15406b89b0080ebc9
iamkarenbaldota <43983275+iamkarenbaldota@users.noreply.github.com>
2018-10-09 14:04:11 +0530

    Update karan2.py

--- karan2.py
+++ karan2.py
@@ -1,11 +1,11 @@
-def ArmN(x):
+def ArmN(x):                         #define function
  sum=0
  t=x
- while (t>0):    #starts while loop
-    d=t%10      #extracts last digit
-    sum+=d**3      #cube of extracted digit
-    t=t//10      #gives quotent of the input
- if sum==x: 
+ while (t>0):                        #starts while loop
+    d=t%10                           #extracts last digit
+    sum+=d**3                        #cube of extracted digit
+    t=t//10                          #gives quotent of the input
+ if sum==x:                          #checks if sum equals input
     return "Armstrong Number"
  else:
     return "Not Armstrong Number"
