58b1c7a653fdc489180b943d0ad4e374a8a91a9a
iamkarenbaldota <43983275+iamkarenbaldota@users.noreply.github.com>
2018-10-16 13:57:21 +0530

    Add files via upload

---
+++ karanAmazon.py
@@ -0,0 +1,15 @@
+while 1:
+    msgbox("E-mall!")
+
+    msg ="what do you wish to buy??"
+    title = "Domains"
+    choices = ["Electronics", "Grocery", "Clothing", "Furniture"]
+    choice = choicebox(msg, title, choices)
+    
+    if:
+     choice==Electronics:
+     choises1=["Computers","Laptops","Mobiles","Home Appliences","Accesories"]
+     choise1= choicebox(choises1)
+    elif choise==Grocery:
+     choises2=["""""""""'"] 
+     
