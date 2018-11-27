b68ba879e7f3ff4556b5bbf3db8b00a5d3d60c0b
iamkarenbaldota <43983275+iamkarenbaldota@users.noreply.github.com>
2018-11-27 12:52:08 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,4 +1,5 @@
-t1=int(input("Enter number 1 :"))
+t1=int(input("Enter number 1 :"))       #asks user for input
 t2=int(input("Enter number 2 :"))
-print("Average of two numbers is: ")
-print((t1+t2)/2)
+print("Average of two numbers is: ")    #prints the given statement
+print((t1+t2)/2)                        #prints the final output
+ 
